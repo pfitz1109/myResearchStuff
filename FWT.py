d49595e494034d5c77510bd198ff1e7821441517
pfitzge4 <pfitzge4@nd.edu>
2026-05-15 15:41:51 -0400

    trying out an FWT program

---
+++ FWT.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def FWT(p,j):
+    hSquiggle = np.zeros((2**j*p+1,2**(j+1)*p+1))
+    gSquiggle = np.zeros((2**j*p,2**(j+1)*p+1))
+
+    
+
+    return np.append(hSquiggle,gSquiggle,axis=0)
